0a3e602c67ab5c46c7b1c4cd3bbe71986300a25f
AbugaAroni <abugaaroni@gmail.com>
2020-11-01 21:16:12 +0300

    add business class to models

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -34,7 +34,7 @@ class neighbourhood(models.Model):
 
 class user(models.Model):
     name = models.ForeignKey(User,on_delete=models.CASCADE)
-    national_id = models.CharField(max_length=20)
+    nati = models.CharField(max_length=20)
     nhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
 
     def __str__(self):
@@ -45,3 +45,27 @@ class user(models.Model):
 
     def delete_user(self):
         self.delete()
+
+class business(models.Model):
+    name = models.CharField(max_length=20)
+    user_owner = models.ForeignKey(User,on_delete=models.CASCADE)
+    neighbourhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
+    business_email = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+    def create_business(self):
+        self.save()
+
+    def delete_business(self):
+        self.delete()
+
+    @classmethod
+    def find_business(cls,busid):
+        biz = cls.objects.get(id = busid)
+        return biz
+        
+    def update_business(self):
+        name = self.name
+        self.name = name
