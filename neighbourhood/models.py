01fec0a326a03a2dc6b97bf603a7ca48970c3c08
AbugaAroni <abugaaroni@gmail.com>
2020-11-01 20:33:33 +0300

    add tbasic essting for neighbourhood class

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -1,15 +1,15 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Neighbourhood(models.Model):
+
+
+# Create your models here.
+class neighbourhood(models.Model):
     name = models.CharField(max_length=20)
     location  = models.CharField(max_length=50)
-    No_occupants = models.IntegerField(default=0)
-    admin = models.CharField(max_length=20)
+    no_occupants = models.IntegerField(default=0)
+    admin = models.ForeignKey(User,on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
--- neighbourhood/tests.py
+++ neighbourhood/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
-
+from models import neighborhood
+from django.db import models
+from django.contrib.auth.models import User
 # Create your tests here.
+class NeighborhoodTestClass(TestCase):
+        # Set up method
+    def setUp(self):
+        self.abuga = User(username="rick", password="password")
+        self.abuga.save()
+        self.neighbourhood= Neighbourhood(name = "MC31", location= "langata", no_occupants = 1, admin = self.abuga)
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.neighbourhood,neighbourhood))
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.neighbourhood.save_Neighbourhood()
+        testsaved = neighbourhood.objects.all()
+        self.assertTrue(len(testsaved) > 0)
+
+    # Testing Delete Method
+    def test_delete_method(self):
+        self.neighbourhood.save_Neighbourhood()
+        testsaved = neighbourhood.objects.all()
+        self.assertTrue(len(testsaved) > 0)
+
+        self.neighbourhood.delete_Neighbourhood()
+        testdelete = neighbourhood.objects.filter(name="MC31")
+        self.assertEqual(len(testdelete), 0)
