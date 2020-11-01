12414aec630df3da197bd0db3d43dd80c66041f1
AbugaAroni <abugaaroni@gmail.com>
2020-11-01 21:44:37 +0300

    confgure urls.py views and ip4.urls

--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Create your views here.
+def home(request):
+    allprojects = "x
+    return render(request, 'homepage.html', {"projects":allprojects})
