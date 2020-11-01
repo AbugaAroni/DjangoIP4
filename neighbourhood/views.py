eb145b5f75423561c03875808afdf3382b8e75f4
AbugaAroni <abugaaroni@gmail.com>
2020-11-01 22:39:23 +0300

    add django authentication

--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 # Create your views here.
 def home(request):
-    allprojects = "x
+    allprojects = "x"
     return render(request, 'homepage.html', {"projects":allprojects})
