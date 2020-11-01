895129a55150380a765ef0d10a47a64bdd66696b
AbugaAroni <abugaaroni@gmail.com>
2020-11-01 20:35:07 +0300

    add db conection

--- ip4/settings.py
+++ ip4/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'neighrbourhood',
+    'neighbourhood',
     'bootstrap',
     'django.contrib.admin',
     'django.contrib.auth',
