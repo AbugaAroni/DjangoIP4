ed221701b34735f3420c9f71d365b104106ddecb
AbugaAroni <abugaaroni@gmail.com>
2020-11-01 18:30:30 +0300

    add basic requirments and add bootstrap to settings.py

--- ip4/settings.py
+++ ip4/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'neighrbourhood',
+    'bootstrap',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
